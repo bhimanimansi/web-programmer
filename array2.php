51f584196b39a6389aa4f3b27a3f8e582bede716
bhimanimansi <155127264+bhimanimansi@users.noreply.github.com>
2024-01-25 08:48:03 +0530

    Add files via upload

---
+++ array2.php
@@ -0,0 +1,8 @@
+<?php
+
+$b=array(array("rollno"=>1,"name"=>"mansi","city"=>"rajkot"),array(1,"abc",5454));
+
+print_r($b);
+echo $b[1][2];
+
+?>
---
+++ array3.php
@@ -0,0 +1,13 @@
+<?php
+
+
+$student_master_b1=array(array("rollno"=>1,"name"=>"atmiya","city"=>"rajkot"),
+array("rollno"=>1,"name"=>"atmiya","city"=>"rajkot"),
+array("rollno"=>1,"name"=>"atmiya","city"=>"rajkot"));
+
+                                                     
+
+print_r($student_master_b1);
+
+
+?>
